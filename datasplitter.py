830f9619fdd741805cf5ad96d073497537e80892
CatherineTaelman <48383489+CatherineTaelman@users.noreply.github.com>
2019-06-18 13:52:52 +0200

    Add files via upload

--- datasplitter.py
+++ datasplitter.py
@@ -2,13 +2,11 @@
 =============================================================================
     Eindhoven University of Technology
 ==============================================================================
-
     Source Name   : datasplitter.py
                     Splits data in different sets and outputs txt files with 
                     paths from ./Data/train/audio
     Author        : Bart van Erp
     Date          : 06/06/2019
-
 ==============================================================================
 """
 
@@ -17,66 +15,68 @@ import os
 import random
 from tqdm import tqdm
 
-# parameters
-filepath_train = 'Data/train'
-validation_percentage = 10
-testing_percentage = 10
-classes = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence']
-
-# open .txt files
-f_train = open(filepath_train+'/lists/train_set.txt',"w+")
-f_test = open(filepath_train+'/lists/test_set.txt',"w+")
-f_validation = open(filepath_train+'/lists/validation_set.txt',"w+")
-
-# loop through all available folders
-folders = os.listdir(filepath_train+'/audio')
-for k1 in tqdm(folders, "processing folders"):
+def datasplitter():
+    # parameters
+    filepath_train = 'Data/train'
+    validation_percentage = 10
+    testing_percentage = 10
+    classes = ['yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go', 'silence']
     
-    # skip the background noise folder
-    if k1 == "_background_noise_":
-        continue
+    # open .txt files
+    f_train = open(filepath_train+'/lists/train_set.txt',"w+")
+    f_test = open(filepath_train+'/lists/test_set.txt',"w+")
+    f_validation = open(filepath_train+'/lists/validation_set.txt',"w+")
     
-    # loop through the unknown files
-    elif k1 not in classes:
-        files = os.listdir(filepath_train+'/audio/'+k1)
+    # loop through all available folders
+    folders = os.listdir(filepath_train+'/audio')
+    for k1 in tqdm(folders, "processing folders"):
         
-        for k2 in files:
-            
-            #calculate probability
-            prob = random.random()*100
-            
-            #classify
-            if prob > validation_percentage+testing_percentage:
-                #train set
-                f_train.write('unknown/'+k2+'\n')
-            elif prob < testing_percentage:
-                #test set
-                f_test.write('unknown/'+k2+'\n')
-            else:
-                #validation set
-                f_validation.write('unknown/'+k2+'\n')
+        # skip the background noise folder
+        if k1 == "_background_noise_":
+            continue
         
+        # loop through the unknown files
+        elif k1 not in classes:
+            files = os.listdir(filepath_train+'/audio/'+k1)
+            
+            for k2 in files:
+                
+                #calculate probability
+                prob = random.random()*100
+                
+                #classify
+                if prob > validation_percentage+testing_percentage:
+                    #train set
+                    f_train.write(k1+'/'+k2+'\n')
+                elif prob < testing_percentage:
+                    #test set
+                    f_test.write(k1+'/'+k2+'\n')
+                else:
+                    #validation set
+                    f_validation.write(k1+'/'+k2+'\n')
+            
+        
+        # Loop through all available files
+        else:
+            files = os.listdir(filepath_train+'/audio/'+k1)
+            for k2 in files:
+                
+                # calculate probability
+                prob = random.random()*100
+                
+                # classify
+                if prob > validation_percentage+testing_percentage:
+                    # train set
+                    f_train.write(k1+'/'+k2+'\n')
+                elif prob < testing_percentage:
+                    # test set
+                    f_test.write(k1+'/'+k2+'\n')
+                else:
+                    # validation set
+                    f_validation.write(k1+'/'+k2+'\n')
     
-    # Loop through all available files
-    else:
-        files = os.listdir(filepath_train+'/audio/'+k1)
-        for k2 in files:
-            
-            # calculate probability
-            prob = random.random()*100
-            
-            # classify
-            if prob > validation_percentage+testing_percentage:
-                # train set
-                f_train.write(k1+'/'+k2+'\n')
-            elif prob < testing_percentage:
-                # test set
-                f_test.write(k1+'/'+k2+'\n')
-            else:
-                # validation set
-                f_validation.write(k1+'/'+k2+'\n')
-
-# close files
-f_train.close()
-f_test.close()
-f_validation.close()
+    # close files
+    f_train.close()
+    f_test.close()
+    f_validation.close()
+    return
